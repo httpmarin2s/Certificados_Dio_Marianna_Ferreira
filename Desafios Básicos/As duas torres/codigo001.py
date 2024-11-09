8ba80f592880ab3c99cab2afd69797fa2860db87
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-11-09 15:38:51 -0300

    Create codigo001.py

---
+++ Desafios Básicos/As duas torres/codigo001.py
@@ -0,0 +1,15 @@
+# Abaixo segue um exemplo de código que você pode ou não utilizar
+entrada = input().split()
+# esse código aqui vai repartir cada um dos itens 
+
+distancia = int(entrada[0])
+diametro1 = int(entrada[1])
+diametro2 = int(entrada[2])
+
+if 0 < distancia < 10000 and 0 < diametro1 < 100 and 0 < diametro2 < 100: 
+  def ICM(distancia, diametro1, diametro2): 
+    calculo = distancia/(diametro1 + diametro2)
+    return (f"{calculo:.2f}")
+
+resultado_icm = ICM(distancia, diametro1, diametro2)
+print(resultado_icm)
